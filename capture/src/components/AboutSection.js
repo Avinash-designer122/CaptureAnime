17cf3f343458a02d2eb3e48025e6996dc5e3f3a5
Avinash-designer122 <avinash4773@gmail.com>
2022-05-08 00:50:03 +0530

    just commit

--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -4,7 +4,7 @@ const AboutSection = () => {
     return (
         <div className='description'>
             <div className="title">
-                
+                <h2>hello world</h2>
             </div>
         </div>
     );
